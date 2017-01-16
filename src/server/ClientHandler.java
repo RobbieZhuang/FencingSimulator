4282421f79c66464b8c4a92961cce0bf3f2e6bfa
cyclopsicle <andi.al.li135@gmail.com>
2017-01-16 17:56:43 -0500

    Updated Client Handler to receive bitstring
    Inputs are now sent from client as a bitstring, for example: 010101.
    1 - key is pressed
    0 - key is not pressed.
    Matches bitstring from right to left: WASDFG

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -71,54 +71,88 @@ public class ClientHandler implements Runnable {
             try {
             	Thread.sleep(10);
                 if (input.ready()) {
-                    message = input.readLine();
+                	message = input.readLine();
                     System.out.println("Message received from " + ": " + message);
-                    if (message != null && message.charAt(0) != '#') { // Make sure its not a login request
-                    	// W - Up
-                    	// A - Left
-                    	// S - Down
-                    	// D - Right
-                    	// F - Attack
-                    	// G - Jump
-                        if (message.charAt(0) == 'W'){
-                        	keys [0] = true;
-                        }
-                        else if (message.charAt(0) == 'A'){
-                        	keys [1] = true;
-                        }
-                        else if (message.charAt(0) == 'S'){
-                        	keys [2] = true;
-                        }
-                        else if (message.charAt(0) == 'D'){
-                        	keys [3] = true;
-                        }
-                        else if (message.charAt(0) == 'F'){
-                        	keys [4] = true;
-                        }
-                        else if (message.charAt(0) == 'G'){
-                        	keys [5] = true;
-                        }
+                    
+                    byte received = Byte.valueOf(message);
+                    
+                    if ((received & (1<<0)) != 0){
+                    	keys [0] = true;
+                    } else {
+                    	keys [0] = false;
+                    }                    
+                    if ((received & (1<<1)) != 0){
+                    	keys [1] = true;
+                    } else {
+                    	keys [1] = false;
+                    }                    
+                    if ((received & (1<<2)) != 0){
+                    	keys [2] = true;
+                    } else {
+                    	keys [2] = false;
+                    }                    
+                    if ((received & (1<<3)) != 0){
+                    	keys [3] = true;
+                    } else {
+                    	keys [3] = false;
+                    }                    
+                    if ((received & (1<<4)) != 0){
+                    	keys [4] = true;
+                    } else {
+                    	keys [4] = false;
+                    }                    
+                    if ((received & (1<<5)) != 0){
+                    	keys [5] = true;
+                    } else {
+                    	keys [5] = false;
                     }
-                    else if (message.charAt(0) == '#'){
-                        if (message.charAt(1) == 'W'){
-                        	keys [0] = false;
-                        }
-                        else if (message.charAt(1) == 'A'){
-                        	keys [1] = false;
-                        }
-                        else if (message.charAt(1) == 'S'){
-                        	keys [2] = false;
-                        }
-                        else if (message.charAt(1) == 'D'){
-                        	keys [3] = false;
-                        }
-                        else if (message.charAt(1) == 'F'){
-                        	keys [4] = false;
-                        }
-                        else if (message.charAt(1) == 'G'){
-                        	keys [5] = false;
-                        }
-                    }
+                    
+//                    if (message != null && message.charAt(0) != '#') { // Make sure its not a login request
+//                    	// W - Up
+//                    	// A - Left
+//                    	// S - Down
+//                    	// D - Right
+//                    	// F - Attack
+//                    	// G - Jump
+//                        if (message.charAt(0) == 'W'){
+//                        	keys [0] = true;
+//                        }
+//                        else if (message.charAt(0) == 'A'){
+//                        	keys [1] = true;
+//                        }
+//                        else if (message.charAt(0) == 'S'){
+//                        	keys [2] = true;
+//                        }
+//                        else if (message.charAt(0) == 'D'){
+//                        	keys [3] = true;
+//                        }
+//                        else if (message.charAt(0) == 'F'){
+//                        	keys [4] = true;
+//                        }
+//                        else if (message.charAt(0) == 'G'){
+//                        	keys [5] = true;
+//                        }
+//                    }
+//                    else if (message.charAt(0) == '#'){
+//                        if (message.charAt(1) == 'W'){
+//                        	keys [0] = false;
+//                        }
+//                        else if (message.charAt(1) == 'A'){
+//                        	keys [1] = false;
+//                        }
+//                        else if (message.charAt(1) == 'S'){
+//                        	keys [2] = false;
+//                        }
+//                        else if (message.charAt(1) == 'D'){
+//                        	keys [3] = false;
+//                        }
+//                        else if (message.charAt(1) == 'F'){
+//                        	keys [4] = false;
+//                        }
+//                        else if (message.charAt(1) == 'G'){
+//                        	keys [5] = false;
+//                        }
+//                    }
                 }
             } catch (Exception e) {
                 System.out.println("*** Error receiving message ***");
