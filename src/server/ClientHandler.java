1daa8f00866440b064773bcf6bc2a350b818b57a
335768479 <335768479@316R20386LJGHX1.EDU.YRDSB.NET>
2017-01-17 11:54:14 -0500

    Updated bitstring for server

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -18,6 +18,8 @@ import java.net.Socket;
 public class ClientHandler implements Runnable {
 
     // Declaring variables
+
+    
     private Socket connectionSocket;
     private BufferedReader input;
     private PrintWriter output;
@@ -44,17 +46,12 @@ public class ClientHandler implements Runnable {
      * @throws IOException to prevent exceptions caused by input and output streams
      */
     ClientHandler(Socket connectionSocket, int playerID) throws IOException {
-        this.status = 1;
+        status = 1;
         this.connectionSocket = connectionSocket;
-        this.running = true;
+        input = new BufferedReader(new InputStreamReader(this.connectionSocket.getInputStream()));
+        output = new PrintWriter(this.connectionSocket.getOutputStream());
+        running = true;
         this.playerID = playerID;
-        try {
-            this.input = new BufferedReader(new InputStreamReader(this.connectionSocket.getInputStream()));
-			this.output = new PrintWriter(this.connectionSocket.getOutputStream());
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
     }
 
     /**
@@ -62,7 +59,6 @@ public class ClientHandler implements Runnable {
      */
     @Override
     public void run() {
-
     	write(""+playerID);
     	
         System.out.println("Running new ServerReceiver");
@@ -75,54 +71,88 @@ public class ClientHandler implements Runnable {
             try {
             	Thread.sleep(10);
                 if (input.ready()) {
-                    message = input.readLine();
+                	message = input.readLine();
                     System.out.println("Message received from " + ": " + message);
-                    if (message != null && message.charAt(0) != '#') { // Make sure its not a login request
-                    	// W - Up
-                    	// A - Left
-                    	// S - Down
-                    	// D - Right
-                    	// F - Attack
-                    	// G - Jump
-                        if (message.charAt(0) == 'W'){
-                        	keys [0] = true;
-                        }
-                        else if (message.charAt(0) == 'A'){
-                        	keys [1] = true;
-                        }
-                        else if (message.charAt(0) == 'S'){
-                        	keys [2] = true;
-                        }
-                        else if (message.charAt(0) == 'D'){
-                        	keys [3] = true;
-                        }
-                        else if (message.charAt(0) == 'F'){
-                        	keys [4] = true;
-                        }
-                        else if (message.charAt(0) == 'G'){
-                        	keys [5] = true;
-                        }
+                    
+                    byte received = Byte.valueOf(message);
+                    
+                    if ((received & (1<<0)) != 0){
+                    	keys [0] = true;
+                    } else {
+                    	keys [0] = false;
+                    }                    
+                    if ((received & (1<<1)) != 0){
+                    	keys [1] = true;
+                    } else {
+                    	keys [1] = false;
+                    }                    
+                    if ((received & (1<<2)) != 0){
+                    	keys [2] = true;
+                    } else {
+                    	keys [2] = false;
+                    }                    
+                    if ((received & (1<<3)) != 0){
+                    	keys [3] = true;
+                    } else {
+                    	keys [3] = false;
+                    }                    
+                    if ((received & (1<<4)) != 0){
+                    	keys [4] = true;
+                    } else {
+                    	keys [4] = false;
+                    }                    
+                    if ((received & (1<<5)) != 0){
+                    	keys [5] = true;
+                    } else {
+                    	keys [5] = false;
                     }
-                    else if (message.charAt(0) == '#'){
-                        if (message.charAt(1) == 'W'){
-                        	keys [0] = false;
-                        }
-                        else if (message.charAt(1) == 'A'){
-                        	keys [1] = false;
-                        }
-                        else if (message.charAt(1) == 'S'){
-                        	keys [2] = false;
-                        }
-                        else if (message.charAt(1) == 'D'){
-                        	keys [3] = false;
-                        }
-                        else if (message.charAt(1) == 'F'){
-                        	keys [4] = false;
-                        }
-                        else if (message.charAt(1) == 'G'){
-                        	keys [5] = false;
-                        }
-                    }
+                    
+//                    if (message != null && message.charAt(0) != '#') { // Make sure its not a login request
+//                    	// W - Up
+//                    	// A - Left
+//                    	// S - Down
+//                    	// D - Right
+//                    	// F - Attack
+//                    	// G - Jump
+//                        if (message.charAt(0) == 'W'){
+//                        	keys [0] = true;
+//                        }
+//                        else if (message.charAt(0) == 'A'){
+//                        	keys [1] = true;
+//                        }
+//                        else if (message.charAt(0) == 'S'){
+//                        	keys [2] = true;
+//                        }
+//                        else if (message.charAt(0) == 'D'){
+//                        	keys [3] = true;
+//                        }
+//                        else if (message.charAt(0) == 'F'){
+//                        	keys [4] = true;
+//                        }
+//                        else if (message.charAt(0) == 'G'){
+//                        	keys [5] = true;
+//                        }
+//                    }
+//                    else if (message.charAt(0) == '#'){
+//                        if (message.charAt(1) == 'W'){
+//                        	keys [0] = false;
+//                        }
+//                        else if (message.charAt(1) == 'A'){
+//                        	keys [1] = false;
+//                        }
+//                        else if (message.charAt(1) == 'S'){
+//                        	keys [2] = false;
+//                        }
+//                        else if (message.charAt(1) == 'D'){
+//                        	keys [3] = false;
+//                        }
+//                        else if (message.charAt(1) == 'F'){
+//                        	keys [4] = false;
+//                        }
+//                        else if (message.charAt(1) == 'G'){
+//                        	keys [5] = false;
+//                        }
+//                    }
                 }
             } catch (Exception e) {
                 System.out.println("*** Error receiving message ***");
--- src/server/Keys.java
+++ src/server/Keys.java
@@ -15,9 +15,6 @@ public class Keys {
 
 	public void setKeys(boolean[] keys) {
 		this.keys = keys;
-		for (int i = 0; i < keys.length; i++){
-			System.out.println(keys[i] + " ");
-		}
 	}
 
 	public int getPlayerID() {
